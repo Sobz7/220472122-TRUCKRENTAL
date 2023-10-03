baba8d243ed8c069a21e5aa42e72ff1d8f661c97
Sobz7 <101484085+Sobz7@users.noreply.github.com>
2023-10-03 23:36:10 +0200

    ALL CHANGES FOR THE PAYMENT AND RENTAL CLASSES.

--- src/main/java/za/ac/cput/service/PaymentService.java
+++ src/main/java/za/ac/cput/service/PaymentService.java
@@ -1,7 +1,8 @@
 package za.ac.cput.service;
 
 import org.springframework.stereotype.Service;
-import za.ac.cput.TruckRental.domain.Payment;
+import za.ac.cput.domain.Payment;
+
 
 @Service
 public interface PaymentService extends IService<Payment, Integer> {
--- src/main/java/za/ac/cput/service/RentalService.java
+++ src/main/java/za/ac/cput/service/RentalService.java
@@ -1,6 +1,7 @@
 package za.ac.cput.service;
+
 import org.springframework.stereotype.Service;
-import za.ac.cput.TruckRental.domain.Rental;
+import za.ac.cput.domain.Rental;
 
 @Service
 public interface RentalService extends IService<Rental, Integer>{
